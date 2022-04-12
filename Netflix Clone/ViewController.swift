29c22f171b8203b272f6205e2767fd3db66f62ff
Marlon Marques <marlon.belohd@gmail.com>
2022-04-12 20:39:52 -0300

    Initial Commit and start Project Config

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Marlon Marques on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
