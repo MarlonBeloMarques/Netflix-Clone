20e80083807d2566607728c75a0705d4170ba48d
Marlon Marques <marlon.belohd@gmail.com>
2022-04-15 19:00:03 -0300

    Create CollectionViewTableViewCell and use in HomeViewController by splitting into sections

---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Marlon Marques on 15/04/22.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
