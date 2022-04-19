79cdfae8c79e6210f43be2f590b98e49825bc5db
Marlon Marques <marlon.belohd@gmail.com>
2022-04-19 20:59:10 -0300

    Create HeroHeaderUIView and use in header of HomeViewController

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,34 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Marlon Marques on 19/04/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "HeroImage")
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
