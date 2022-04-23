f46b2c1f5d4898cd48202722b51a224defcfa0a9
Marlon Marques <marlon.belohd@gmail.com>
2022-04-23 10:52:54 -0300

    call API to get Trending Movies

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,39 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Marlon Marques on 22/04/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "7df7fc93558d6d28d27a55709112d945"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedToGetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrandingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
